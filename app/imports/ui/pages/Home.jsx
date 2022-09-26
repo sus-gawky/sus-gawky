0cf07bf88179eaf5c414331e0553fdb9839ecaf9
Gavin Peng <blackbullet54321@gmail.com>
2022-09-25 23:54:21 -1000

    implement home page

--- app/imports/ui/pages/Home.jsx
+++ app/imports/ui/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Carousel, Col, Container, Form, Row } from 'react-bootstrap';
 import { motion } from 'framer-motion';
 
 const Home = () => {
@@ -14,16 +14,17 @@ const Home = () => {
     }
     return fakeGoals;
   };
+
   return (
-    <Container className="justify-content-center w-100">
+    <Container style={{ backgroundColor: '#F5F5F5' }}>
       <Row className="fredoka-one">
-        <Col>Welcome, Gavin Peng</Col>
+        <Col className="d-flex justify-content-center">Welcome, Gavin Peng</Col>
       </Row>
-      <Row className="mt-3">
-        <Col>
+      <Row className="mt-4">
+        <Col className="d-flex justify-content-center">
           <Form>
             <>
-              <div className="fredoka-one goals">
+              <div className="fredoka-one goals mt-5">
                 Goals
                 <Button className="mx-3" size="sm" onClick={() => (setModal(true))}>Add Goal</Button>
               </div>
@@ -49,23 +50,52 @@ const Home = () => {
             </>
           </Form>
         </Col>
-        <Col>
+        <Col className="d-flex justify-content-center">
           <div className="home-picture" />
         </Col>
       </Row>
-      <Row className="mt-3">
-        {/* TODO: Make this a list */}
-        <Col>List</Col>
+      <Row className="mt-5">
+        {/* TODO: Make this have a dotted line connecting the buttons */}
+        <Col className="d-flex justify-content-center">
+          <Button style={{ backgroundColor: '#60E831FF', border: 'none' }}>Button 1</Button>
+        </Col>
+        <Col className="d-flex justify-content-center">
+          <Button style={{ backgroundColor: '#2AA404FF', border: 'none' }}>Button 2</Button>
+        </Col>
+        <Col className="d-flex justify-content-center">
+          <Button style={{ backgroundColor: '#257206FF', border: 'none' }}>Button 3</Button>
+        </Col>
       </Row>
-      <Row className="mt-3">
+      <Row className="mt-5 mb-3">
         {/* TODO: Make this a carousel */}
-        <Col><div className="score-card" /></Col>
-        <Col>
+        <Col className="d-flex justify-content-center">
+          <div className="score-card">
+            <Carousel>
+              <Carousel.Item interval={1500}>
+                <div className="score-card">
+                  Green card one
+                </div>
+              </Carousel.Item>
+              <Carousel.Item interval={1500}>
+                <div className="score-card">
+                  Green card two
+                </div>
+              </Carousel.Item>
+              <Carousel.Item interval={1500}>
+                <div className="score-card">
+                  Green card three
+                </div>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </Col>
+        <Col className="d-flex justify-content-center mt-5">
           <div>
-            Tip of the day
+            Some motivational speech
           </div>
         </Col>
       </Row>
+
     </Container>
   );
 };
