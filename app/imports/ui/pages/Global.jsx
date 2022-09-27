361ab05d15e52613a344123e2c75ff22f6b8938c
Gavin Peng <blackbullet54321@gmail.com>
2022-09-26 16:46:16 -1000

    Implement global page

--- app/imports/ui/pages/Global.jsx
+++ app/imports/ui/pages/Global.jsx
@@ -1,38 +1,115 @@
-import React, { useEffect, useCallback, useState } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import { Spinner } from 'react-bootstrap';
-import apiKey from '../../../apiKey.json';
+import React, { useEffect, useState, useMemo } from 'react';
+import { GoogleMap, Polygon, useLoadScript } from '@react-google-maps/api';
+import { ListGroup, Offcanvas, Spinner } from 'react-bootstrap';
+import axios from 'axios';
 
 const Global = () => {
-  const [map, setMap] = useState(null);
-  const center = {
-    lat: -3.745,
-    lng: -38.523,
-  };
-  const { isLoaded } = useJsApiLoader({
+  const [locationData, setLocationData] = useState({});
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [polygon, setPolygon] = useState([]);
+  const { isLoaded } = useLoadScript({
     id: 'google-map-script',
-    googleMapsApiKey: apiKey['react-maps-api-key'],
+    googleMapsApiKey: 'AIzaSyASlpfLxUWXlRshNWQGvZWAX9kGP63yj7s',
   });
-  useEffect(async () => {
-    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json${new URLSearchParams({
-      address: 98732,
-      key: apiKey['react-maps-api-key'],
-    })}`, { method: 'GET' });
-    console.log(response);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+          params: {
+            address: 96822,
+            key: 'AIzaSyASlpfLxUWXlRshNWQGvZWAX9kGP63yj7s',
+          },
+        });
+        setLocationData(response);
+        setLat(response.data.results[0].geometry.location.lat);
+        setLng(response.data.results[0].geometry.location.lng);
+        const polygonResponse = await axios.get('https://nominatim.openstreetmap.org/search.php', {
+          params: {
+            q: 'waimalu',
+            polygon_geojson: 1,
+            format: 'json',
+          },
+        });
+        const filterGeoJsonType = polygonResponse.data.filter((data) => (data.geojson.type === 'Polygon' || data.geojson.type === 'MultiPolygon'));
+        await filterGeoJsonType.map((polygonObj) => setPolygon((prev) => ([...prev, polygonObj.geojson.coordinates])));
+      } catch (e) {
+        console.log(`The error is: ${e}`);
+      }
+    }
+    fetchData();
   }, []);
-  // Retrieved from https://www.npmjs.com/package/@react-google-maps/api
-  const onLoad = useCallback(() => {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
+  console.log(locationData);
+  const retrievePaths = () => {
+    const returnArr = [];
+
+    for (const coordinates of polygon) {
+      // combine multi polygon into one
+      let s = [];
+      if (coordinates.length > 1) {
+        for (const cods of coordinates) {
+          s = [...s, ...cods];
+        }
+      } else {
+        s = [...coordinates];
+      }
+      const part = [];
+      for (const coordinate of s) {
+        for (const c of coordinate) {
+          part.push({ lat: parseFloat(c[1]), lng: parseFloat(c[0]) });
+        }
+      }
+      returnArr.push(part);
+    }
+    return returnArr;
+  };
+  const center = useMemo(() => ({ lat, lng }), [lat, lng]);
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={{ width: '100%', height: '30px' }}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-    />
+    <>
+      <GoogleMap
+        mapContainerStyle={{ width: '100vw', height: '100vh' }}
+        center={center}
+        zoom={13}
+      >
+        <>
+          {retrievePaths().map((place, index) => (
+            <Polygon
+              paths={place}
+              key={index}
+              onClick={() => (setShowOverlay(!showOverlay))}
+              options={{
+                fillColor: 'lightgreen',
+                fillOpacity: 0.5,
+                strokeColor: 'black',
+                strokeOpacity: 0.5,
+                strokeWeight: 1,
+                clickable: true,
+                draggable: false,
+                editable: false,
+                geodesic: false,
+                zIndex: 1,
+              }}
+            />
+          ))}
+        </>
+      </GoogleMap>
+      <Offcanvas show={showOverlay} onHide={() => (setShowOverlay(false))} placement="end" name="end" backdrop={false}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Sustainability Habits</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <ListGroup>
+            <ListGroup.Item>Cras justo odio</ListGroup.Item>
+            <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+            <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+            <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+            <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          </ListGroup>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+
   ) : <Spinner animation="border" role="status" />;
 };
 
