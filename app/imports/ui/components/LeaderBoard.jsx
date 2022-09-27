a54fe96495240e848aff7f65f3d6b9a2027e1c27
William Liang <williamliang808@gmail.com>
2022-09-26 16:09:11 -1000

    Makes minor improvements to navbar

--- app/imports/ui/components/LeaderBoard.jsx
+++ app/imports/ui/components/LeaderBoard.jsx
@@ -1,179 +1,119 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import trophy from './trophy';
+import { EmojiAngry, EmojiNeutral, EmojiSmile, Trophy } from 'react-bootstrap-icons';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const LeaderBoard = () => (
   <Carousel variant="dark" className="center-block">
     <Carousel.Item className="leader-body">
-      <div>
-        <div className="leaderboard">
-          <h1>
-            <svg className="ico-cup">
-              <use xlinkHref="#cup" />
-            </svg>
-            Total Score
-          </h1>
-          <ul>
-            <li>
-              <mark>You</mark>
-              <small>100</small>
-            </li>
-          </ul>
-          <ol>
-            <li>
-              <mark>Jerry Wood</mark>
-              <small>315</small>
-            </li>
-            <li>
-              <mark>Brandon Barnes</mark>
-              <small>301</small>
-            </li>
-            <li>
-              <mark>Raymond Knight</mark>
-              <small>292</small>
-            </li>
-            <li>
-              <mark>Trevor McCormick</mark>
-              <small>245</small>
-            </li>
-            <li>
-              <mark>Andrew Fox</mark>
-              <small>203</small>
-            </li>
-          </ol>
-        </div>
-        <svg style={{ display: 'none' }}>
-          <symbol
-            id="cup"
-            x="0px"
-            y="0px"
-            width="25px"
-            height="26px"
-            viewBox="0 0 25 26"
-            enableBackground="new 0 0 25 26"
-            xmlSpace="preserve"
-          >
-            <path
-              fill="#F26856"
-              d={trophy}
-            />
-          </symbol>
-        </svg>
+      <div className="leaderboard">
+        <h1>
+          <Trophy fill="#B2D2A4" />
+          <span> Total Score </span>
+          <EmojiSmile className="float-end" />
+        </h1>
+        <ul>
+          <li>
+            <mark>You</mark>
+            <small>100</small>
+          </li>
+        </ul>
+        <ol>
+          <li>
+            <mark>Jerry Wood</mark>
+            <small>315</small>
+          </li>
+          <li>
+            <mark>Brandon Barnes</mark>
+            <small>301</small>
+          </li>
+          <li>
+            <mark>Raymond Knight</mark>
+            <small>292</small>
+          </li>
+          <li>
+            <mark>Trevor McCormick</mark>
+            <small>245</small>
+          </li>
+          <li>
+            <mark>Andrew Fox</mark>
+            <small>203</small>
+          </li>
+        </ol>
       </div>
     </Carousel.Item>
     <Carousel.Item className="leader-body">
-      <div>
-        <div className="leaderboard">
-          <h1>
-            <svg className="ico-cup">
-              <use xlinkHref="#cup" />
-            </svg>
-            Food Score
-          </h1>
-          <ul>
-            <li>
-              <mark>You</mark>
-              <small>100</small>
-            </li>
-          </ul>
-          <ol>
-            <li>
-              <mark>Jerry Wood</mark>
-              <small>315</small>
-            </li>
-            <li>
-              <mark>Brandon Barnes</mark>
-              <small>301</small>
-            </li>
-            <li>
-              <mark>Raymond Knight</mark>
-              <small>292</small>
-            </li>
-            <li>
-              <mark>Trevor McCormick</mark>
-              <small>245</small>
-            </li>
-            <li>
-              <mark>Andrew Fox</mark>
-              <small>203</small>
-            </li>
-          </ol>
-        </div>
-        <svg style={{ display: 'none' }}>
-          <symbol
-            id="cup"
-            x="0px"
-            y="0px"
-            width="25px"
-            height="26px"
-            viewBox="0 0 25 26"
-            enableBackground="new 0 0 25 26"
-            xmlSpace="preserve"
-          >
-            <path
-              fill="#F26856"
-              d={trophy}
-            />
-          </symbol>
-        </svg>
+      <div className="leaderboard">
+        <h1>
+          <Trophy fill="#B2D2A4" />
+          Food Score
+          <EmojiAngry className="float-end" />
+        </h1>
+        <ul>
+          <li>
+            <mark>You</mark>
+            <small>100</small>
+          </li>
+        </ul>
+        <ol>
+          <li>
+            <mark>Jerry Wood</mark>
+            <small>315</small>
+          </li>
+          <li>
+            <mark>Brandon Barnes</mark>
+            <small>301</small>
+          </li>
+          <li>
+            <mark>Raymond Knight</mark>
+            <small>292</small>
+          </li>
+          <li>
+            <mark>Trevor McCormick</mark>
+            <small>245</small>
+          </li>
+          <li>
+            <mark>Andrew Fox</mark>
+            <small>203</small>
+          </li>
+        </ol>
       </div>
     </Carousel.Item>
     <Carousel.Item className="leader-body">
-      <div>
-        <div className="leaderboard">
-          <h1>
-            <svg className="ico-cup">
-              <use xlinkHref="#cup" />
-            </svg>
-            Travel Score
-          </h1>
-          <ul>
-            <li>
-              <mark>You</mark>
-              <small>100</small>
-            </li>
-          </ul>
-          <ol>
-            <li>
-              <mark>Jerry Wood</mark>
-              <small>315</small>
-            </li>
-            <li>
-              <mark>Brandon Barnes</mark>
-              <small>301</small>
-            </li>
-            <li>
-              <mark>Raymond Knight</mark>
-              <small>292</small>
-            </li>
-            <li>
-              <mark>Trevor McCormick</mark>
-              <small>245</small>
-            </li>
-            <li>
-              <mark>Andrew Fox</mark>
-              <small>203</small>
-            </li>
-          </ol>
-        </div>
-        <svg style={{ display: 'none' }}>
-          <symbol
-            id="cup"
-            x="0px"
-            y="0px"
-            width="25px"
-            height="26px"
-            viewBox="0 0 25 26"
-            enableBackground="new 0 0 25 26"
-            xmlSpace="preserve"
-          >
-            <path
-              fill="#F26856"
-              d={trophy}
-            />
-          </symbol>
-        </svg>
+      <div className="leaderboard">
+        <h1>
+          <Trophy fill="#B2D2A4" />
+          Travel Score
+          <EmojiNeutral className="float-end" />
+        </h1>
+        <ul>
+          <li>
+            <mark>You</mark>
+            <small>100</small>
+          </li>
+        </ul>
+        <ol>
+          <li>
+            <mark>Jerry Wood</mark>
+            <small>315</small>
+          </li>
+          <li>
+            <mark>Brandon Barnes</mark>
+            <small>301</small>
+          </li>
+          <li>
+            <mark>Raymond Knight</mark>
+            <small>292</small>
+          </li>
+          <li>
+            <mark>Trevor McCormick</mark>
+            <small>245</small>
+          </li>
+          <li>
+            <mark>Andrew Fox</mark>
+            <small>203</small>
+          </li>
+        </ol>
       </div>
     </Carousel.Item>
   </Carousel>
--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { useTracker } from 'meteor/react-meteor-data';
 import { NavLink } from 'react-router-dom';
@@ -6,7 +6,7 @@ import { Nav, Image, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import {
   BoxArrowRight,
   Building,
-  Globe2,
+  Globe2, House,
   PersonCircle,
   Stars,
 } from 'react-bootstrap-icons';
@@ -19,12 +19,13 @@ const NavBar = () => {
     currentUser: Meteor.user() ? Meteor.user().username : '',
   }), []);
 
+  const [open, setOpen] = useState(false);
   return (
     <div>
-      <ProSidebar collapsed className="position-fixed">
+      <ProSidebar collapsed={open} className="position-fixed">
         <Menu iconShape="circle">
-          <MenuItem icon={<Image src="images/gawkysaurTurtleFancy.png" width="50em" />}>
-            <Nav.Link as={NavLink} to="/" />
+          <MenuItem icon={<Image src="images/gawkysaurTurtleFancy.png" width="50em" />} onClick={() => setOpen(!open)}>
+            <h4 className="text-center">Close</h4>
           </MenuItem>
           {currentUser ? ([
             <OverlayTrigger
@@ -32,8 +33,19 @@ const NavBar = () => {
               delay={{ show: 250, hide: 200 }}
               overlay={<Tooltip>Profile</Tooltip>}
             >
+              <MenuItem className="mt-3" icon={<House size={50} />}>
+                <Nav.Link as={NavLink} to="/homepage" />
+                <h4 className="text-center">Home</h4>
+              </MenuItem>
+            </OverlayTrigger>,
+            <OverlayTrigger
+              placement="right"
+              delay={{ show: 250, hide: 200 }}
+              overlay={<Tooltip>Profile</Tooltip>}
+            >
               <MenuItem className="mt-3" icon={<PersonCircle size={50} />}>
                 <Nav.Link as={NavLink} to="/" />
+                <h4 className="text-center">Profile</h4>
               </MenuItem>
             </OverlayTrigger>]) :
             ([
@@ -44,6 +56,7 @@ const NavBar = () => {
               >
                 <MenuItem className="mt-3" icon={<PersonCircle size={50} />}>
                   <Nav.Link as={NavLink} to="/signin" />
+                  <h4 className="text-center">Login</h4>
                 </MenuItem>
               </OverlayTrigger>])}
           {currentUser ? ([
@@ -54,6 +67,7 @@ const NavBar = () => {
             >
               <MenuItem className="mt-3" icon={<Stars size={50} />}>
                 <Nav.Link as={NavLink} to="/" />
+                <h4 className="text-center">Praise</h4>
               </MenuItem>
             </OverlayTrigger>,
             <OverlayTrigger
@@ -63,6 +77,7 @@ const NavBar = () => {
             >
               <MenuItem className="mt-3" icon={<Building size={50} />}>
                 <Nav.Link as={NavLink} to="/add" />
+                <h4 className="text-center">Neighborhood</h4>
               </MenuItem>
             </OverlayTrigger>,
             <OverlayTrigger
@@ -72,6 +87,7 @@ const NavBar = () => {
             >
               <MenuItem className="mt-3" icon={<Globe2 size={50} />}>
                 <Nav.Link as={NavLink} to="/" />
+                <h4 className="text-center">World View</h4>
               </MenuItem>
             </OverlayTrigger>,
             <OverlayTrigger
@@ -81,6 +97,7 @@ const NavBar = () => {
             >
               <MenuItem className="position-absolute bottom-0 mb-4" icon={<BoxArrowRight size={50} />}>
                 <Nav.Link as={NavLink} to="/signout" />
+                <h4 className="position-absolute bottom-0 mb-3 ms-5 float-start">Logout</h4>
               </MenuItem>
             </OverlayTrigger>,
           ]) : ''}
