783d148a39e29d4f232e0d81bc488061b96aa179
Gavin Peng <blackbullet54321@gmail.com>
2022-09-27 21:55:01 -1000

    add city to setup

--- app/imports/api/functions/functions.js
+++ app/imports/api/functions/functions.js
@@ -26,32 +26,32 @@ class Functions {
     return returnString;
   };
 
-  static avPoints = (zipCode, users) => {
-    let result = users.filter(user => user.zipCode === zipCode);
+  static avPoints = (city, users) => {
+    let result = users.filter(user => user.city === city);
     result = result.map((user) => user.points).reduce((a, b) => a + b, 0) / result.length;
     return result;
   };
 
-  static avFoodScore = (zipCode, users) => {
-    let result = users.filter(user => user.zipCode === zipCode);
+  static avFoodScore = (city, users) => {
+    let result = users.filter(user => user.city === city);
     result = result.map((user) => user.foodScore).reduce((a, b) => a + b, 0) / result.length;
     return result;
   };
 
-  static avMiscScore = (zipCode, users) => {
-    let result = users.filter(user => user.zipCode === zipCode);
+  static avMiscScore = (city, users) => {
+    let result = users.filter(user => user.city === city);
     result = result.map((user) => user.miscScore).reduce((a, b) => a + b, 0) / result.length;
     return result;
   };
 
-  static avTransportationScore = (zipCode, users) => {
-    let result = users.filter(user => user.zipCode === zipCode);
+  static avTransportationScore = (city, users) => {
+    let result = users.filter(user => user.city === city);
     result = result.map((user) => user.transportationScore).reduce((a, b) => a + b, 0) / result.length;
     return result;
   };
 
-  static avFullScore = (zipCode, users) => {
-    let result = users.filter(user => user.zipCode === zipCode);
+  static avFullScore = (city, users) => {
+    let result = users.filter(user => user.city === city);
     result = result.map((user) => user.fullScore).reduce((a, b) => a + b, 0) / result.length;
     return result;
   };
