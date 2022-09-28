69e52e26f76eab42ab925d76c0404659b9aea362
Alyssia Chen <84742988+alyssia-chen@users.noreply.github.com>
2022-09-28 11:40:46 -1000

    Landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,21 +3,20 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <div>
-    <Container id="landing-page" fluid className="py-3">
-      <div style={{ zIndex: 2 }}>
-        <h1>Welcome to Gawkysaur!</h1>
-      </div>
-      <div style={{ position: 'fixed', bottom: 30, zIndex: 1 }}>
-        <Image className="translateGawkysaur" src="images/gawkysaurTurtleFancy.png" style={{ height: '30%', width: '30%', zIndex: -1 }} />
-      </div>
-      <Col>
-        <Image src="images/backgroundTransparent.png" style={{ width: '100%', height: '100%', float: 'right' }} />
-        <Row><Image src="https://w7.pngwing.com/pngs/892/738/png-transparent-cartoon-green-mountain-cartoon-decoration-angle-triangle-christmas-decoration.png" style={{ width: '50%', position: 'fixed', right: '0' }} /></Row>
-        <Row>      <Image style={{ position: 'fixed', bottom: '-10' }} src="https://i.dlpng.com/static/png/6405906_preview.png" />
-        </Row>
-      </Col>
-    </Container>
+  <div id="landing-page">
+    <div style={{ float: 'right', paddingRight: '5em' }}>
+      <h1>Welcome to Gawkysaur!</h1>
+    </div>
+    <div style={{ position: 'fixed', bottom: 30, zIndex: 2 }}>
+      <Image className="translateGawkysaur" src="images/gawkysaurTurtleFancy.png" style={{ height: '20%', width: '20%' }} />
+    </div>
+    <Col>
+
+      <Image src="images/backgroundTransparent.png" style={{ width: '100%', height: '50%', float: 'right' }} />
+      <Row style={{ paddingRight: 0, float: 'right' }}>
+        <Image style={{ position: 'fixed', bottom: '0', zIndex: 1 }} src="images/waterPretty.png" />
+      </Row>
+    </Col>
   </div>
 );
 
