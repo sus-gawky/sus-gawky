42b0ac66d4942d7489371e5e9ca05ebca9ed7a20
Kobeyarai808 <kobeyarai@gmail.com>
2022-09-28 14:16:03 -1000

    Finished landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,20 +3,16 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <div id="landing-page">
+  <div id="landing-page" style={{ backgroundImage: `url('../../images/background.png')` }}>
     <div style={{ float: 'right', paddingRight: '5em' }}>
       <h1>Welcome to Gawkysaur!</h1>
     </div>
     <div style={{ position: 'fixed', bottom: 30, zIndex: 2 }}>
       <Image className="translateGawkysaur" src="images/gawkysaurTurtleFancy.png" style={{ height: '20%', width: '20%' }} />
     </div>
-    <Col>
-
-      <Image src="images/backgroundTransparent.png" style={{ width: '100%', height: '50%', float: 'right' }} />
-      <Row style={{ paddingRight: 0, float: 'right' }}>
-        <Image style={{ position: 'fixed', bottom: '0', zIndex: 1 }} src="images/waterPretty.png" />
-      </Row>
-    </Col>
+    {/*<Col>*/}
+    {/*  <Image src="images/background.png" style={{ width: '100%', height: '50%', float: 'right' }} />*/}
+    {/*</Col>*/}
   </div>
 );
 
